b9e65ed671656d22c4cdbd1dd6823500b06eb661
JWrightDev7 <james.wright01@stclairconnect.ca>
2022-01-30 13:41:28 -0500

    Initial Commit

---
+++ J_Wright_MyHobbies/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Content } from './helper-files/content-interface';
+import { ContentList } from './helper-files/content-list';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'J_Wright_MyHobbies';
+  
+}
